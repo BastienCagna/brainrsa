45633b20fa9f33546b8aa724cdbf817571471996
cagna.b <bastien.CAGNA@univ-amu.fr>
2019-11-26 15:55:17 +0100

    Initial commit. Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='drsa',
+    version='',
+    packages=['drsa'],
+    url='https://github.com/BastienCagna/drsa',
+    license='',
+    author='Bastien Cagna',
+    author_email='',
+    description=''
+)
